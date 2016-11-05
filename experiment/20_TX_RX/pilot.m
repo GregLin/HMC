7dc6828b0150fed7af86b4ced12e0cccff5656c7
Yenting (Greg) Lin <yenting0322@gmail.com>
2016-11-04 17:36:50 -0700

    modify parallel

--- experiment/20_TX_RX/pilot.m
+++ experiment/20_TX_RX/pilot.m
@@ -6,11 +6,15 @@ path(path, '/home/yenting/Dropbox/Sync_matlab/IEEE_CI/dataset')
 path(path, '/home/yenting/Dropbox/Sync_matlab/IEEE_CI/cost_function')
 path(path, '/home/yenting/Dropbox/Sync_matlab/IEEE_CI/toolbox')
 
+% define parallel pools
+parpool('local',8)
+
 % define parameters
 num_obj = 2; 
 delta = 5* 1e-3; 
 Tau = 20;
 max_iter = 20;
+p = 25;
 filename = string('debug_result.txt');
 
 % load data
@@ -18,7 +22,7 @@ load('dataset_20_TX_RX.mat','-mat');
 
 % run simulation
 HMC_simulation(Tx, Rx, num_obj, sim_times, delta, Tau, ...
-                        max_iter, filename);
+                        p, max_iter, filename);
 
 % [output_model, cost] = HMC_reconstruction(Tx, Rx, num_obj, sim_times, delta, epsilon) ; 
 
